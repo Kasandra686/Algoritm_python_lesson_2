5cf4a11cfc49ddf21122e3a70048088e9f644d7c
Вера <verazverev@yandex.ru>
2020-04-08 23:29:19 +0500

    ок

---
+++ 4.py
@@ -0,0 +1,14 @@
+# Найти сумму n элементов следующего ряда чисел: 1 -0.5 0.25 -0.125 ...Количество элементов (n) вводится с клавиатуры.
+
+
+def summ(n, x=1):
+    if n == 1:
+        return x
+    else:
+        result = x + summ(n - 1, x / -2)
+        return result
+
+
+n = int(input('Введите n '))
+result = summ(n)
+print(result)
